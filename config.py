97cad30a2cc447599f53fed283bf776183e5bb8b
Subhankar De <desubhankar94@gmail.com>
2020-07-03 23:54:48 +0530

    App Settings

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'qwerty'
+    ENV = '***'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ENV = 'PROD'
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    ENV = 'STG'
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    ENV = 'DEV'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    ENV = 'TEST'

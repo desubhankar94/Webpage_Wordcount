90eef3fcec881b7d5adc5d6cbc74c248d61f4f0f
Subhankar De <desubhankar94@gmail.com>
2020-07-09 23:34:53 +0530

    add index.html

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
-def hello():
-    return "Hello World !"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 @app.route('/<name>')
 def hello_name(name):
